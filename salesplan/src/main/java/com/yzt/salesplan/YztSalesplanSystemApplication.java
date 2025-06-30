78199aa748390fd3a14aa4f3522d6b3d95e98e0c
lanzejun <lanzejun@cn.ibm.com>
2025-06-30 08:42:02 +0800

    Add salesplan project

---
+++ salesplan/src/main/java/com/yzt/salesplan/YztSalesplanSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yzt.salesplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YztSalesplanSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YztSalesplanSystemApplication.class, args);
+	}
+
+}
---
+++ salesplan/src/test/java/com/yzt/salesplan/YztSalesplanSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzt.salesplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YztSalesplanSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
